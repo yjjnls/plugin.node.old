4f23753eccfb16f5579f351ff5f3092cbf5fe78c
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-06-05 16:55:38 +0800

    .

--- test.py
+++ test.py
@@ -1,28 +1,12 @@
+from __future__ import absolute_import, division, print_function
+import sys
 import os
 import re
 import semver
+import devutils
+import argparse
+from devutils.shell import check_call,prompt
 
-def check_call(cmd, cmd_dir=None):
-    import sys
-    import shlex
-    import subprocess
-
-    if isinstance(cmd, str):
-        cmd = shlex.split(cmd)
-    process = subprocess.Popen(cmd, cwd=cmd_dir,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE, shell=True)
-    output, err = process.communicate()
-    if process.poll():
-        raise Exception("runing command %s failed"%cmd)
-
-    if sys.stdout.encoding:
-        output = output.decode(sys.stdout.encoding)
-
-    if sys.stdout.encoding:
-        err = err.decode(sys.stdout.encoding)
-
-    return output,err
 
 class ReleaseManager(object):
 
@@ -78,11 +62,90 @@ class ReleaseManager(object):
         f.write( content )
         f.close()
 
-    def _get_commit_count(self):
+    def buildno(self):
+        return self._get_last_tag_commit_count()
 
-        out,err = check_call('git log --since="Oct 27 9:16:10 2017 +0800"  --pretty=oneline')
-        print out
-        print len(out.split('\n'))
+
+    def parse(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("command")
+
+
+    def _build(self,args):
+        print ('RUN build')
+
+    def _bump_version(self,args):
+        if self._has_uncommit():
+            print('you have some files not commited. please commit all of them, before bump the version.')
+            os.system('git status ')
+            return
+
+        oldver = self._conanfile_version
+        ver    = semver.inc( oldver, args.release,loose = True)
+        
+        res = prompt('''
+        bump the version %s => %s
+        src/addon/src/version.h
+        conanfile.py
+        docs/release-notes.md
+        version related infomation will update to the new version.
+        '''%(oldver,ver), ['yes','no'])
+        if res == 'no':
+            print('cancel the version bumps.')
+            return
+
+        self._update_version( ver )
+        res = prompt('''
+        bump the version %s => %s
+        please check below files 
+
+        src/addon/src/version.h
+        conanfile.py
+        docs/release-notes.md
+        yes => commit and tag this release
+        no => give up the commit, you have to revert change file manually.
+
+        '''%(oldver,ver), ['yes','no'])
+        if res == 'no':
+            return
+
+        check_call('git commit -m bumps to version %s'%ver)
+        check_call('git tag %s -m bumps to version %s'%ver)
+
+
+    def _has_uncommit(self):
+        out,err = check_call('git status -s --show-stash')
+        out = out.strip('\n')
+        print (out,'<-----')
+        if out:            
+            return True
+        return False
+
+
+
+
+    def run(self, argv):
+        if len(argv) == 0:
+            argv = ['build']
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers()
+        
+        # create the parser for the "foo" command
+        builder = subparsers.add_parser('build')
+        builder.add_argument('--with-nvm', type=bool, default=False)        
+        builder.set_defaults(func=self._build)
+        
+        # create the parser for the "bar" command
+        bump_version = subparsers.add_parser('bump-version')
+        bump_version.add_argument('release', choices=['major', 'minor', 'patch'])
+        bump_version.set_defaults(func=self._bump_version)
+        
+        # parse the args and call whatever function was selected
+        args = parser.parse_args(argv)
+        args.func(args)
+
+        
+
 
 
 
@@ -90,9 +153,28 @@ class ReleaseManager(object):
         
 
 
-rm = ReleaseManager()
+#rm = ReleaseManager()
+#
+##rm._update_version("1.2.3.4s")
+#
+##rm._get_commit_count()
+##semver.inc('1.2.3-dev.')
+#print '---------------------'
+#print rm.buildno()
+#print rm._get_last_tag()
+#print rm._get_tag_commit_id( rm._get_last_tag())
+#print '---------------------'
 
-#rm._update_version("1.2.3.4s")
 
-rm._get_commit_count()
-semver.inc('1.2.3-dev.')
+if __name__ == '__main__':    
+    rm =ReleaseManager()
+    rm.run( sys.argv[1:] )
+    #import argparse
+    #import sys
+    #print sys.argv
+    #
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("echo",default = None)
+    #args = parser.parse_args(sys.argv[1:])
+    #print args.echo
+    
