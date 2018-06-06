30302994be2119f2772784db3661a33a7613dccc
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-06-06 16:19:26 +0800

    bumps to version 0.5.2

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ def _UNUSED(*param):
 
 class NodePlugin(ConanFile):
     name = "plugin.node"
-    version = "0.5.2-dev"
+    version = "0.5.2"
     url = "https://github.com/Mingyiz/plugin.node"
     homepage = url
     description = "Node.js addon for c plugin dynamic."
