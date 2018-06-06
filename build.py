dcf44eee64e8b327aecad49d3139bdda69357c3f
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-06-06 10:49:04 +0800

    .

--- build.py
+++ build.py
@@ -80,7 +80,7 @@ def build():
         CONAN_UPLOAD_ONLY_WHEN_STABLE = False
         CONAN_REFERENCE = '%s/%s'%(PACKAGE_NAME,version)
 
-        f = open(os.path.join(__dir__,'/src/addon/version.h', 'wb'))
+        f = open(os.path.join(__dir__,'/src/addon/version.h') ,'wb')
         f.write(r'#define __VERSION__ "%s\n"'%version)
         f.close()#
 
